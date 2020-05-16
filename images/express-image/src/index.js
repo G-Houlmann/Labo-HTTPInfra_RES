065760591418ab6507a2c7858936c3130925cfd1
G-Houlmann <gildas.houlmann@gmail.com>
2020-05-16 21:49:48 +0200

    added express image

---
+++ images/express-image/src/index.js
@@ -0,0 +1,14 @@
+var Chance = require('chance');
+var chance = new Chance();
+
+var express = require('express');
+var app = express();
+
+
+app.get('/', function(req, res){
+    res.send("Hello RES");
+});
+
+app.listen(3000, function(){
+    console.log("Accepting HTTP requests on port 3000");
+});
