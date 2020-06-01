30a6e7ed90eff42e10fc7c2787700a79b6bb1f19
G-Houlmann <gildas.houlmann@gmail.com>
2020-06-01 21:39:28 +0200

    added basic load balancing

--- images/apache-reverse-proxy/template/config-template.php
+++ images/apache-reverse-proxy/template/config-template.php
@@ -1,15 +1,28 @@
 <?php
-    $dynamicApp = getEnv('DYNAMIC_APP');
-    $staticApp = getEnv('STATIC_APP');
+	$dynamicApp1 = getEnv('DYNAMIC_APP_1');
+	$dynamicApp2 = getEnv('DYNAMIC_APP_2');
+	$staticApp1 = getEnv('STATIC_APP_1');
+	$staticApp2 = getEnv('STATIC_APP_2');
 ?>
 
 <VirtualHost *:80>
 	ServerName demo.res.ch
+
+	<Proxy "balancer://dynamicServers">
+		BalancerMember 'http://<?php print "$dynamicApp1"; ?>'
+		BalancerMember 'http://<?php print "$dynamicApp2"; ?>'
+	</Proxy>
+
+	<Proxy "balancer://staticServers">
+		BalancerMember 'http://<?php print "$staticApp1"; ?>'
+		BalancerMember 'http://<?php print "$staticApp2"; ?>'
+	</Proxy>
 	
-	ProxyPass '/api/students/' 'http://<?php print "$dynamicApp"?>/'
-	ProxyPassReverse '/api/students/' 'http://<?php print "$dynamicApp"?>/'
 	
-	ProxyPass '/' 'http://<?php print "$staticApp"?>/'
-	ProxyPassReverse '/' 'http://<?php print "$staticApp"?>/'
+	ProxyPass '/api/students/' 'balancer://dynamicServers/'
+	ProxyPassReverse '/api/students/' 'balancer://dynamicServers/'
+	
+	ProxyPass '/' 'balancer://staticServers/'
+	ProxyPassReverse '/' 'balancer://staticServers/'
 </VirtualHost>
 
